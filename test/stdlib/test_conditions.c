62fe7e6a74320aa58093e1ecf262478c93a54b00
Rui Fernandes <ruipedro16@protonmail.com>
2024-06-03 14:01:28 +0200

    remove some tests

--- test/stdlib/test_conditions.c
+++ test/stdlib/test_conditions.c
@@ -16,7 +16,6 @@
 #endif
 
 extern uint8_t cond_u32_a_below_b_and_a_below_c_jazz(uint32_t, uint32_t, uint32_t);
-extern uint8_t cond_u32_geq_u32_u32_eq_u32_jazz(uint32_t, uint32_t, uint32_t, uint32_t);
 
 void test_cond_u32_a_below_b_and_a_below_c(void) {
     uint32_t a, b, c;
@@ -36,73 +35,8 @@ void test_cond_u32_a_below_b_and_a_below_c(void) {
     puts("cond_u32_a_below_b_and_a_below_c: a < b /\\ a < c OK");
 }
 
-void test_cond_u32_geq_u32_u32_eq_u32(void) {
-    uint32_t a, b, c, d;
-    uint8_t r;
-
-    // 1) Random Bytes
-    for (int i = 0; i < TESTS; i++) {
-        randombytes((uint8_t *)&a, sizeof(uint32_t));
-        randombytes((uint8_t *)&b, sizeof(uint32_t));
-        randombytes((uint8_t *)&c, sizeof(uint32_t));
-        randombytes((uint8_t *)&d, sizeof(uint32_t));
-
-        r = cond_u32_geq_u32_u32_eq_u32_jazz(a, b, c, d);
-        assert((a >= b && c == d) ? (r == 1) : (r == 0));
-    }
-
-    // 2) a >= b is true but c == d is false
-    for (int i = 0; i < TESTS; i++) {
-        // a) Case when a > b
-        randombytes((uint8_t *)&a, sizeof(uint32_t));
-        b = a - 1;
-
-        randombytes((uint8_t *)&c, sizeof(uint32_t));
-        d = c + 1;
-
-        r = cond_u32_geq_u32_u32_eq_u32_jazz(a, b, c, d);
-        assert((a >= b && c == d) ? (r == 1) : (r == 0));
-
-        // b) Case when a == b
-        randombytes((uint8_t *)&a, sizeof(uint32_t));
-        b = a;
-
-        randombytes((uint8_t *)&c, sizeof(uint32_t));
-        d = c + 1;
-
-        r = cond_u32_geq_u32_u32_eq_u32_jazz(a, b, c, d);
-        assert((a >= b && c == d) ? (r == 1) : (r == 0));
-    }
-
-    // 3) c == d is true but a >= b is false
-    for (int i = 0; i < TESTS; i++) {
-        randombytes((uint8_t *)&a, sizeof(uint32_t));
-        b = a + 1;
-
-        randombytes((uint8_t *)&c, sizeof(uint32_t));
-        d = c;
-
-        r = cond_u32_geq_u32_u32_eq_u32_jazz(a, b, c, d);
-        assert((a >= b && c == d) ? (r == 1) : (r == 0));
-    }
-
-    // a >= b and c == d are both false
-    for (int i = 0; i < TESTS; i++) {
-        randombytes((uint8_t *)&a, sizeof(uint32_t));
-        b = a + 1;
-        randombytes((uint8_t *)&c, sizeof(uint32_t));
-        d = c + 1;
-
-        r = cond_u32_geq_u32_u32_eq_u32_jazz(a, b, c, d);
-        assert((a >= b && c == d) ? (r == 1) : (r == 0));
-    }
-
-    puts("cond_u32_geq_u32_u32_eq_u32: a >= b /\\ c == d OK");
-}
-
 int main(void) {
     test_cond_u32_a_below_b_and_a_below_c();
-    test_cond_u32_geq_u32_u32_eq_u32();
     puts("CONDITIONS : OK");
     return 0;
 }
