ddfcde8be049ecc661d8048e5bb25b1ddd9e2d4c
Rui Fernandes <ruipedro16@protonmail.com>
2025-02-07 16:35:54 +0100

    remove stuff before pushing to formosa-crypto/xmss

--- bench/compare.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-### Chooses the best line of each csv file 
-### i.e. the one with the smaller overhead 
-
-import pandas as pd
-
-if __name__ == "__main__":
-    raise NotImplementedError     
